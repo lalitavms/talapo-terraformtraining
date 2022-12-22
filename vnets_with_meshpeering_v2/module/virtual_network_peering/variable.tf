8f2fe429dac9b5e288be82ad7540911e13dfd369
lalitavms <107113258+lalitavms@users.noreply.github.com>
2022-12-22 18:50:12 +0700

    create new version with re-structure file and variables (#3)

---
+++ vnets_with_meshpeering_v2/module/virtual_network_peering/variable.tf
@@ -0,0 +1,5 @@
+#variable "name" {}
+variable "resource_group_name" {}
+variable "peerings" {}
+variable "virtual_network_name" {}
+variable "virtual_networks" {}
---
+++ vnets_with_meshpeering_v2/terraform.tfvars
@@ -0,0 +1,2 @@
+env_prefix = "tft"
+location   = "australiaeast"
