49910767e13c96b0400c2f322d7202408332c4cf
lalitavms <lalitav@microsoft.com>
2023-01-16 06:22:13 +0700

    add windows virtual machine

--- vnets_with_meshpeering_v2/global.providers.tf
+++ vnets_with_meshpeering_v2/global.providers.tf
@@ -4,9 +4,17 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.35.0"
     }
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = "1.2.23"
+    }
   }
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
--- vnets_with_meshpeering_v2/main.resourcegroup.tf
+++ vnets_with_meshpeering_v2/main.resourcegroup.tf
@@ -1,12 +1,22 @@
-locals {
-  resource_group_name = "${var.env_prefix}-rg"
+# locals {
+#   resource_group_name = "${var.env_prefix}-rg"
+# }
+
+data "azurecaf_name" "mainrg" {
+  name          = "main"
+  resource_type = "azurerm_resource_group"
+  prefixes      = ["tft"]
+  #  suffixes = ["dev"]
+  #  random_length = 5
+  clean_input = true
 }
 
-resource "azurerm_resource_group" "testrg" {
-  name     = local.resource_group_name
+resource "azurerm_resource_group" "mainrg" {
+  name = data.azurecaf_name.mainrg.result
+  #  name     = local.resource_group_name
   location = var.location
 }
 
-output "resource_group_testrg_out" {
-  value = azurerm_resource_group.testrg
+output "resource_group_mainrg_out" {
+  value = azurerm_resource_group.mainrg
 }
